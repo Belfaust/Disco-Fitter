900e4e714a13ef04c4a04ea80c2eb30d7869b313
Belfaust <Belfaust@users.noreply.github.com>
2020-12-12 00:30:09 +0100

    UI init

---
+++ Assets/Scripts/Models/Customers.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Customer",menuName = "Customer")]
+public class Customers : ScriptableObject
+{ 
+    public Sprite customerSprite;
+}
