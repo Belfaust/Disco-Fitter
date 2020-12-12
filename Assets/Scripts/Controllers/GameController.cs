ebc7816e69668bc1e286fde448f9a939e8f94a7c
Belfaust <Belfaust@users.noreply.github.com>
2020-12-12 02:01:40 +0100

    Beat Display

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,17 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
-    void Start()
+    public static GameController instance;
+    [SerializeField]private float BPM;
+    [SerializeField] private UIController _uiController;
+    private float timer;
+    private float currentTime;
+    private void Start()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
         
+        timer = 1 / (BPM / 60);
+        _uiController.SetMaxBeatValue(timer);
+        StartCoroutine(BeatTimer());
     }
 
-    void Update()
+    private IEnumerator BeatTimer()
     {
-        
+        while (true)
+        {
+            StartCoroutine(BeatShower());
+            yield return new WaitForSeconds(timer);
+        }
+    }
+
+    private IEnumerator BeatShower()
+    {
+         currentTime = timer;
+        while(currentTime>0)
+        {
+            currentTime -= Time.deltaTime;
+            _uiController.DisplayBeat(currentTime);
+            yield return new WaitForSeconds(0);
+        }
     }
 }
---
+++ Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]private Slider BeatShower;
+
+    public void DisplayBeat(float currentTime)
+    {
+        Debug.Log(currentTime);
+        BeatShower.value = currentTime;
+    }
+
+    public void SetMaxBeatValue(float maxValue)
+    {
+        BeatShower.maxValue = maxValue;
+    }
+    void Update()
+    {
+        
+    }
+}
