a06960a5a4db1cae81e72f0a017f98b8e97f1462
Belfaust <Belfaust@users.noreply.github.com>
2020-12-13 00:49:59 +0100

    scene manager

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -150,7 +150,7 @@ public class GameController : MonoBehaviour
         {
             if (_beatCount > 126)
             {
-                SceneManager
+                SceneManager.LoadScene("EndScreen");
             }
             StartCoroutine(BeatShower());
             yield return new WaitForSeconds(timer);
