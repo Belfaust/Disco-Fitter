de792ac0f4921b0cebb61ed67cdbb5cbcc726eed
Juru111 <jrypanasevich@gmail.com>
2020-12-12 14:49:19 +0100

    Credits

---
+++ Assets/Scripts/MenuMenager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMenager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject creditsPanel;
+    private bool isCreditsPanel = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsPanel.SetActive(false);
+        isCreditsPanel = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EnterGameplay()
+    {
+
+    }
+
+    public void OpenCloseSettings()
+    {
+
+    }
+
+    public void RollCredits()
+    {
+        if(isCreditsPanel == false)
+        {
+            creditsPanel.SetActive(true);
+            isCreditsPanel = true;
+        }
+        else
+        {
+            creditsPanel.SetActive(false);
+            isCreditsPanel = false;
+        }
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/RandomOfsetAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomOfsetAnimation : MonoBehaviour
+{
+    Animator m_Animator;
+
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_Animator.SetFloat("offset", Random.Range(0f, 1f));
+    }
+
+    void OnEnable()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_Animator.SetFloat("offset", Random.Range(0f, 1f));
+    }
+}
